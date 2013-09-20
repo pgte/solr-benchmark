acce00f850150bdf1f4d3ac9f756f7f7bbea1297
Pedro Teixeira <pedro.teixeira@gmail.com>
2013-09-20 15:48:19 +0100

    retrieveing proper stats

--- read.js
+++ read.js
@@ -1,31 +1,41 @@
-var bogan = require('boganipsum');
-var solr  = require('solr-client').createClient();
-require('microtime');
-var Benchmark = require('benchmark');
-
+var bogan     = require('boganipsum');
+var solr      = require('solr-client').createClient();
+var microtime = require('microtime');
+var async     = require('async');
+var summary   = require('summary');
 require('http').globalAgent.defaultMaxSockets = Infinity;
-
-var id = 0;
 
 solr.autoCommit = true;
 
-var benchmark = new Benchmark({
-	name: 'write',
-	defer: true,
-	fn: runOne
-});
+var concurrency = Number(process.argv[2]) || 5;
+console.log('concurrency: %d', concurrency);
+
+var count = Number(process.argv[3]) || 10000;
+console.log('count: %d', count);
+
+var start = microtime.now();
+
+var queue = async.queue(runOne, concurrency);
+queue.drain = drain;
+for (var i = 0 ; i < count; i ++) {
+  queue.push(i);
+}
+
+var samples = [];
+
+var id = 0;
 
 var terms = bogan().replace(/\./g, '').split(' ').map(function(s) {
 	return s.toLowerCase();
 }).filter(function(s) {
 	return s.length > 3;
 });
-
 console.log('Using %d search terms', terms.length);
 
 var searches = 0;
 
-function runOne(deferred) {
+function runOne(_, cb) {
+	var start = microtime.now();
 	searches ++;
 	id ++;
 	var term = terms[id % (terms.length)];
@@ -38,21 +48,24 @@ function runOne(deferred) {
 	var query = solr.createQuery().q(query).start(0).rows(10);
 
 	solr.search(query, function(err, results) {
+		var end = microtime.now();
+		var elapsed = end - start;
+		samples.push(elapsed);
 		if (err) throw err;
 		// console.log('results:', results.response.numFound);
-		deferred.resolve();
+		cb();
 	});
 }
 
-benchmark
-.on('complete', function() {
-	console.log('\nResults:\n');
-	console.log('performed searches:', searches);
-	console.log('freq: %d ops / sec', this.hz);
-	console.log('elapsed: %d secs', this.times.elapsed);
-  console.log('mean: %d ms', this.stats.mean * 1000);
-  console.log('variance: +- %d s', this.stats.variance * 1000);
-  console.log('cycles: %d', this.stats.sample.length);
-  console.log('count: %d', this.count);
-})
-.run({async: true, queued: true});
+function drain() {
+	var end = microtime.now();
+	var elapsed = (end - start) / 1e3;
+	var freq = count / elapsed;
+	console.log('elapsed: %d ms', elapsed);
+	console.log('freq: %d ops / sec', freq * 1e3);
+	var s = summary(samples);
+	console.log('mean: %d ms', s.mean() / 1e3);
+	console.log('stddev: +- %d ms', s.sd() / 1e3);
+	console.log('max: %d ms', s.max() / 1e3);
+	console.log('min: %d ms', s.min() / 1e3);
+};
